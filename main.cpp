2ee6f7bfdb894e125aa64e1c66d626724de407d3
livello <livello@bk.ru>
2017-10-14 03:39:00 +0300

    dht22 ds1820b

---
+++ main.cpp
@@ -0,0 +1,300 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+#include "Arduino.h"
+#include <HardwareSerial.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <IPAddress.h>
+#include <WMath.h>
+#include <EthernetServer.h>
+#include "ChibiOS_ARM.h"
+#include "dht.h"
+#include "OneWire.h"
+
+
+constexpr uint8_t pin_onewire   { 35 };
+
+
+#define DHTPIN 33
+
+const uint8_t LED_PIN = 13;
+dht dht_sensor;
+
+OneWire ds(35);
+
+int value = 0;
+SEMAPHORE_DECL(sem, 0);
+
+byte mac[] = {
+        0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 2, 40);
+
+EthernetServer server(80);
+
+int numPins = 4;
+int pins[] = {4, 5, 6, 7};    // Пины для реле
+int pinState[] = {0, 0, 0, 0};  // Состояние пинов
+float h=0, t=0;
+
+struct
+{
+    uint32_t total;
+    uint32_t ok;
+    uint32_t crc_error;
+    uint32_t time_out;
+    uint32_t unknown;
+} stat1 = { 0,0,0,0,0 };
+
+void show_ds18b20(){
+    byte i;
+    byte present = 0;
+    byte type_s;
+    byte data[12];
+    byte addr[8];
+    float celsius, fahrenheit;
+
+    if ( !ds.search(addr)) {
+        Serial.println("No more addresses.");
+        Serial.println();
+        ds.reset_search();
+        delay(250);
+        return;
+    }
+
+    Serial.print("ROM =");
+    for( i = 0; i < 8; i++) {
+        Serial.write(' ');
+        Serial.print(addr[i], HEX);
+    }
+
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+        Serial.println("CRC is not valid!");
+        return;
+    }
+    Serial.println();
+
+    // the first ROM byte indicates which chip
+    switch (addr[0]) {
+        case 0x10:
+            Serial.println("  Chip = DS18S20");  // or old DS1820
+            type_s = 1;
+            break;
+        case 0x28:
+            Serial.println("  Chip = DS18B20");
+            type_s = 0;
+            break;
+        case 0x22:
+            Serial.println("  Chip = DS1822");
+            type_s = 0;
+            break;
+        default:
+            Serial.println("Device is not a DS18x20 family device.");
+            return;
+    }
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+
+    delay(1000);     // maybe 750ms is enough, maybe not
+    // we might do a ds.depower() here, but the reset will take care of it.
+
+    present = ds.reset();
+    ds.select(addr);
+    ds.write(0xBE);         // Read Scratchpad
+
+    Serial.print("  Data = ");
+    Serial.print(present, HEX);
+    Serial.print(" ");
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+        data[i] = ds.read();
+        Serial.print(data[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.print(" CRC=");
+    Serial.print(OneWire::crc8(data, 8), HEX);
+    Serial.println();
+
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s) {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10) {
+            // "count remain" gives full 12 bit resolution
+            raw = (raw & 0xFFF0) + 12 - data[6];
+        }
+    } else {
+        byte cfg = (data[4] & 0x60);
+        // at lower res, the low bits are undefined, so let's zero them
+        if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+        else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+        else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+        //// default is 12 bit resolution, 750 ms conversion time
+    }
+    celsius = (float)raw / 16.0;
+    fahrenheit = celsius * 1.8 + 32.0;
+    Serial.print("  Temperature = ");
+    Serial.print(celsius);
+    Serial.print(" Celsius, ");
+    Serial.print(fahrenheit);
+    Serial.println(" Fahrenheit");
+}
+
+static THD_WORKING_AREA(waThread1, 64);
+
+static THD_FUNCTION(Thread1, arg) {
+
+    while (!chThdShouldTerminateX()) {
+        // Wait for signal from thread 2.
+        chSemWait(&sem);
+
+        // Turn LED off.
+        digitalWrite(LED_PIN, LOW);
+
+        // DISPLAY DATA
+        uint32_t start = micros();
+        int chk = dht_sensor.read22(DHTPIN);
+        uint32_t stop = micros();
+
+        stat1.total++;
+        switch (chk)
+        {
+            case DHTLIB_OK:
+                stat1.ok++;
+                h = dht_sensor.humidity;
+                // Read temperature as Celsius (the default)
+                t = dht_sensor.temperature;
+                Serial.print(h);
+                Serial.print(" %, temperature:");
+                Serial.println(t);
+                break;
+            case DHTLIB_ERROR_CHECKSUM:
+                stat1.crc_error++;
+                Serial.print("Checksum error,\t");
+                break;
+            case DHTLIB_ERROR_TIMEOUT:
+                stat1.time_out++;
+                Serial.print("Time out error,\t");
+                break;
+            default:
+                stat1.unknown++;
+                Serial.print("Unknown error,\t");
+                break;
+        }
+
+        // following function must be called periodically
+      show_ds18b20();
+
+
+
+    }
+}
+
+
+static THD_WORKING_AREA(waThread2, 64);
+
+static THD_FUNCTION(Thread2, arg) {
+    pinMode(LED_PIN, OUTPUT);
+    while (1) {
+        digitalWrite(LED_PIN, HIGH);
+
+        // Sleep for 200 milliseconds.
+        chThdSleepMilliseconds(2000);
+
+        // Signal thread 1 to turn LED off.
+        chSemSignal(&sem);
+
+        // Sleep for 200 milliseconds.
+        chThdSleepMilliseconds(2000);
+    }
+}
+
+void chSetup() {
+
+    // start blink thread
+    chThdCreateStatic(waThread1, sizeof(waThread1),
+                      NORMALPRIO + 2, Thread1, NULL);
+
+    chThdCreateStatic(waThread2, sizeof(waThread2),
+                      NORMALPRIO + 1, Thread2, NULL);
+
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(10);
+
+    // Open serial communications and wait for port to open:
+
+
+    // start the Ethernet connection and the server:
+    Ethernet.begin(mac, ip);
+    server.begin();
+    Serial.print("server is at ");
+    Serial.println(Ethernet.localIP());
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(4, OUTPUT);
+    chBegin(chSetup);
+
+}
+
+void loop() {
+    delay(10);
+
+    EthernetClient client = server.available();
+    if (client) {
+
+        Serial.println("" + client.readString());
+        client.println("HTTP/1.1 200 OK");
+        client.println("Content-Type: text/html");
+        client.println();
+        client.println("<html>");
+        client.println("<head>");
+        client.println("<title>Zelectro. Relay + Ethernet shield.</title>");
+        client.println("</head>");
+        client.println("<body>");
+        client.println("<h3>Zelectro. Relay + Ethernet shield.</h3>");
+        client.println("<form method='post'>");
+        client.print("<div>Relay 1 <input type='checkbox' ");
+        if (pinState[0] == 1)
+            client.print("checked");
+        client.println(" name='r0'></div>");
+        client.print("<div>Relay 2 <input type='checkbox' ");
+        if (pinState[1] == 1)
+            client.print("checked");
+        client.println(" name='r1'></div>");
+        client.print("<div>Relay 3 <input type='checkbox' ");
+        if (pinState[2] == 1)
+            client.print("checked");
+        client.println(" name='r2'></div>");
+        client.print("<div>Relay 4 <input type='checkbox' ");
+        if (pinState[3] == 1)
+            client.print("checked");
+        client.println(" name='r3'></div>");
+        client.println("<input type='submit' value='Refresh'>");
+        client.println("</form>");
+        client.println("</body>");
+        client.println("</html>");
+        client.stop();
+
+    }
+
+}
+
+
+
+
+
+
+
+
