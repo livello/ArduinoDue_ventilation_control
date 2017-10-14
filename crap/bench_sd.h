a77b66f0eee7e953c874025526aba379af7ca053
livello <livello@bk.ru>
2017-10-15 01:48:16 +0300

    1

---
+++ crap/bench_sd.h
@@ -0,0 +1,12 @@
+//
+// Created by livello on 14.10.17.
+//
+
+#ifndef NODEMCU_STOPWATCH_BENCH_SD_H
+#define NODEMCU_STOPWATCH_BENCH_SD_H
+
+#endif //NODEMCU_STOPWATCH_BENCH_SD_H
+
+void error(char* s);
+void bench_sd_loop();
+void bench_sd_setup();
---
+++ crap/modbus_service.cpp
@@ -0,0 +1,7 @@
+//
+// Created by livello on 15.10.17.
+//
+
+#include "modbus_service.h"
+
+
